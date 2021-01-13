08fe9443deed771c7ad02f2e79f1d12d89f5e542
병우 유 <conner903@naver.com>
2021-01-13 12:28:21 +0900

    ADD: HomeView

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -6,6 +6,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.repository.OrderRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,6 +53,45 @@ class OrderServiceTest {
         assertThat(getOrder.getOrderItems().size()).isEqualTo(1);
         assertThat(getOrder.getTotalPrice()).isEqualTo(bookPrice * orderCount);
         assertThat(item.getStockQuantity()).isEqualTo(stockQuantity-orderCount);
+    }
+
+    @Test
+    public void 상품주문_재고수량초과() throws Exception {
+        //Given
+        Member member = createMember();
+        int bookPrice = 10000;
+        int stockQuantity = 10;
+        Item item = createBook("시골 JPA", bookPrice, stockQuantity);
+        int orderCount = 11; //재고보다 많은 수량
+
+        //When
+        NotEnoughStockException e = assertThrows(NotEnoughStockException.class,
+                ()->orderService.order(member.getId(), item.getId(), orderCount));
+
+        //Then
+        assertThat(e.getMessage()).isEqualTo("need more stock");
+    }
+
+    @Test
+    public void 주문취소() {
+
+        //Given
+        Member member = createMember();
+        int bookPrice = 10000;
+        int stockQuantity = 10;
+        Item item = createBook("시골 JPA", bookPrice, stockQuantity);
+        int orderCount =2;
+
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        //When
+        orderService.cancelOrder(orderId);
+
+        //Then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertThat(getOrder.getStatus()).isEqualTo(OrderStatus.CANCEL);
+        assertThat(item.getStockQuantity()).isEqualTo(stockQuantity);
     }
 
     private Item createBook(String name, int price, int stockQuantity) {
